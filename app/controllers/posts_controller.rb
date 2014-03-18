4f2ed6b1d99b4a286f219de5dc115739f7fe633c
Jack Henley <jack.d.henley@gmail.com>
2014-03-18 16:42:46 -0400

    Posts successfully added to database

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,5 +11,14 @@ class PostsController < ApplicationController
   end
 
   def create
+  	@post = Post.create!(post_params)
+  	redirect_to root_path
+  end
+
+  private
+
+
+  def post_params
+    params.require(:post).permit(:name, :email, :message)
   end
 end
