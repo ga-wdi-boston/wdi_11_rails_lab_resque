b7956f78324ee35f47207f63736afb6adbc37742
Becky Bell <rlbell@alum.bu.edu>
2014-03-18 17:43:12 -0400

    added Pony message to be sent upon new user creation

--- app/workers/worker.rb
+++ app/workers/worker.rb
@@ -1,7 +1,8 @@
 class Worker
       @queue = :default
 
-      def self.perform
+      def self.perform(email)
+        Pony.mail(to: email, subject: "Thanks!", body: "You signed the guestbook")
 
       end
 end
