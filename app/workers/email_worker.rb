56b840437a45ae51f9ebc105d51f390de9591d0c
Janice Smith <janice.k.n.smith@gmail.com>
2014-03-18 17:49:32 -0400

    updating the message sent by the worker

--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -10,7 +10,7 @@ class EmailWorker
 
     puts "working on it .... from the email_worker ...."
     #  this is just running this - not yet taking the args from the form for this - but seems to be working on its own
-    Pony.mail(to: email, subject: subject, body: body)
+    Pony.mail(to: email, subject: " your message was about: #{subject}", body: "thank you for signing the guest book")
   end
 
 end
