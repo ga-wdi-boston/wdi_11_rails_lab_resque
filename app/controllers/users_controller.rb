ed74d8d10f362462667df232736e6b1ef344a713
K Mak <karen.mak@stx.oxon.org>
2014-03-18 16:45:30 -0400

    Keep passwords/credentials out of version control and use Sendgrid on Heroku, Email can be sent to a user thanking them for signing the guestbook

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    Pony.mail(to: @user.email, subject: "You've left a mark!", body: "Thank you for signing our guestbook!")
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'Thank you for signing our guestbook!' }
+        format.json { render action: 'show', status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, :email, :post)
+    end
+end
