fec1c78ec228564abcd804905f6711e307292273
Mike Stone <mikestone14@gmail.com>
2014-03-18 17:00:02 -0400

    Added styling and from address for emails

--- app/workers/send_email.rb
+++ app/workers/send_email.rb
@@ -4,6 +4,6 @@ class SendEmail
   # Redis looks for perform
   def self.perform(email)
     sleep 1
-    Pony.mail(to: email, subject: 'Thanks for your post', body: 'Thank you for posting in our guest book. Hope you had a great time!')
+    Pony.mail(to: email, from: 'theparty@partytime.com', subject: 'Thanks for your post', body: 'Thank you for posting in our guest book. Hope you had a great time!')
   end
 end
