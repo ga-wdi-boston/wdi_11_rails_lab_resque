079cb42352cd55188fd71b61139a127c205d1753
Jack Henley <jack.d.henley@gmail.com>
2014-03-18 16:46:13 -0400

    Added destroy for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
   	@posts = Post.all
   end
@@ -15,8 +17,19 @@ class PostsController < ApplicationController
   	redirect_to root_path
   end
 
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.json { head :no_content }
+    end
+  end
+
   private
 
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:name, :email, :message)
