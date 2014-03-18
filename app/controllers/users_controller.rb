bd665c727174b5970bd3688dbf7de34a4761851f
K Mak <karen.mak@stx.oxon.org>
2014-03-18 16:54:51 -0400

    email is sent using a background task with Resque

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -11,12 +10,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    Resque.enqueue(EmailUserWorker, @user.email)
 
-    Pony.mail(to: @user.email, subject: "You've left a mark!", body: "Thank you for signing our guestbook!")
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'Thank you for signing our guestbook!' }
+        format.html { redirect_to :users, notice: 'Thank you for signing our guestbook!' }
         format.json { render action: 'show', status: :created, location: @user }
       else
         format.html { render action: 'new' }
@@ -26,11 +25,6 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name, :email, :post)
