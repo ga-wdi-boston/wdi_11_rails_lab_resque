1be0847eaf60a44892375a0382cfb9fe362138a3
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-03-19 09:35:03 -0400

    Model relationship, form partial, index view

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140319125434) do
+ActiveRecord::Schema.define(version: 20140319132124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "notes", force: true do |t|
-    t.text     "name"
-    t.text     "email"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
