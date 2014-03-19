5dc6beddec7e9f8cb6ee27eff74868244a2581dd
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-03-19 09:00:24 -0400

    Added 'notes' scaffold & mailer

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140318211130) do
+ActiveRecord::Schema.define(version: 20140319125434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "notes", force: true do |t|
+    t.text     "name"
+    t.text     "email"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
